b6cae7a9e836d676a69c2fb5c2728dc83aac28a0
Hao Phan <haophan120193@gmail.com>
2024-10-18 06:55:51 -0400

    first commit 10-18-24

---
+++ src/containers/App.js
@@ -0,0 +1,52 @@
+import facebookicon from '../facebook-brand.svg';
+import googleicon from '../google-brand.svg';
+
+function App() {
+  return (
+    
+    <div className="container"> 
+      <div className="login-content rounded-5  mt-5 p-3  row  ">
+        <div className="text-login col-12 text-center mt-2">Login</div>
+
+        <div className="col-12 form-group login-input">
+          <label>User Name:</label>
+          <input type="text" className="form-control mb-2" placeholder="Enter user name"></input>
+        </div>
+        
+        <div>
+
+          <label>PassWord:</label>
+          <input type="password" className="form-control mb-2" placeholder="Enter password"></input>
+
+        </div>
+        <div className=" col-12 text-center">
+          <button className="btn btn-secondary">Login</button>
+        </div>
+
+        <div className="text-forgot-password col-12">
+
+          <span>Forgot your password</span>
+
+        </div>
+
+        <div className="col-12 text-center">
+
+            <span className="text-orLogin-with">Or Login with:</span>
+
+        </div>
+        <div className="col-12 social-login text-center">
+
+          <img className='facebookicon ' src={facebookicon} />
+          <img className='googleicon ' src={googleicon} />
+           
+        </div>
+      </div>
+
+    </div>
+
+      
+    
+  );
+}
+
+export default App;
