2fb5ed413fe397334748ebaf769590e89feac5e4
Hao Phan <haophan120193@gmail.com>
2024-10-20 14:59:41 -0400

    update login api from get to post

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -3,12 +3,51 @@ import facebookicon from '../../assets/images/facebook-brand.svg';
 import googleicon from '../../assets/images/google-brand.svg';
 import './Login.scss';
 
+import {handleLoginApi} from '../../services/userService';
 class Login extends Component{
     constructor(props){
         super(props);
+        this.state ={
+            email: '',
+            password: '',
+        }
     }
 
+    handleOnchangeUserName = (e)=>{
+        
+        this.setState({
+            email: e.target.value,
+        })
+        
+    }
+
+    handleOnchangePassword = (e)=>{
+        
+        this.setState({
+            password: e.target.value,
+        })
+        
+    }
+
+    handleLogin= async()=>{
+
+
+        console.log(this.state);
+
+       try{
+        await handleLoginApi(this.state.email, this.state.password);
+        
+
+       }catch(e){
+        console.log(e);
+       }
+        
+    }
+
+
+
     render(){
+
         return(
 
             <div className="container"> 
@@ -17,17 +56,24 @@ class Login extends Component{
 
                     <div className="col-12 form-group login-input">
                     <label>User Name:</label>
-                    <input type="text" className="form-control mb-2" placeholder="Enter user name"></input>
+                    <input type="text" className="form-control mb-2" 
+                    placeholder="Enter user name" 
+                    value={this.state.email}
+                    onChange={(e)=> this.handleOnchangeUserName(e)}
+                    ></input>
                     </div>
                     
                     <div>
 
                     <label>PassWord:</label>
-                    <input type="password" className="form-control mb-2" placeholder="Enter password"></input>
+                    <input type="password" className="form-control mb-2" placeholder="Enter password"
+                    onChange={(e)=> this.handleOnchangePassword(e)}
+                    value={this.state.password}></input>
 
                     </div>
                     <div className=" col-12 text-center">
-                    <button className="btn btn-secondary">Login</button>
+                    <button className="btn btn-secondary"
+                    onClick={()=>this.handleLogin()}>Login</button>
                     </div>
 
                     <div className="text-forgot-password col-12">
@@ -44,7 +90,7 @@ class Login extends Component{
                     <div className="col-12 social-login text-center">
 
                     <img className='facebookicon ' src={facebookicon} />
-                    <img className='googleicon ' src={googleicon} />
+                    <img className='googleicon ' src={googleicon}/>
                     
                     </div>
                 </div>
